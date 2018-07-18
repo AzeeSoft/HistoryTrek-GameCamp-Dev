dc44de19938920655c3e452c53cb1422b292700c
SAU Game Camp <saugamecamp@gmail.com>
2018-07-18 16:30:51 -0500

    Adds Main Menu

---
+++ Assets/Jake/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public GameObject CreditsImage;
+	public Image Credits;
+
+	// Use this for initialization
+	void Start () {
+		CreditsImage.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartGame() {
+		
+	}
+
+	public void QuitGame() {
+	
+	}
+
+	public void ShowCredits() {
+		CreditsImage.SetActive (true); 
+	}
+
+}
