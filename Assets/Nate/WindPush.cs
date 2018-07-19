8736886dde77b9de0633d941d5eb93c9c63656f1
SAU Game Camp <saugamecamp@gmail.com>
2018-07-19 12:01:21 -0500

    Wind Blocker Stops Wind

--- Assets/Nate/WindPush.cs
+++ Assets/Nate/WindPush.cs
@@ -17,14 +17,18 @@ public class WindPush : MonoBehaviour {
 	void Update () {
 		
 	}
-
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Lightning") {
+			Debug.Log ("Lost A Life");
+		}
+	}
 	void OnTriggerStay2D(Collider2D other){
-		if (other.tag == "WindPipe") {
+		if (other.tag == "WindPipe" && WindOff == false) {
 			rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y + WindSpeed);
 		}
 	}
 	void OnTriggerExit2D(Collider2D other){
-		if (other.tag == "WindPipe") {
+		if (other.tag == "WindPipe" && WindOff == false) {
 			rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y - DecreaseSpeed);
 		}
 	}
