c800c757009e7a6c84c28a546777a3174b7e3a1d
SAU Game Camp <saugamecamp@gmail.com>
2018-07-19 14:39:29 -0500

    added character animations

--- Assets/Character Controller/CharacterMovement.cs
+++ Assets/Character Controller/CharacterMovement.cs
@@ -14,6 +14,10 @@ public class CharacterMovement : MonoBehaviour {
 	public float groundCheckTolerance;
 
 
+
+	bool iswalking = false;
+
+
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D>();
@@ -27,23 +31,27 @@ public class CharacterMovement : MonoBehaviour {
 
 		float Horizontal = Input.GetAxis ("Horizontal");
 
+		iswalking = Horizontal != 0;
+		animator.SetBool ("idle", !iswalking);
+		animator.SetBool ("walk", iswalking);
+
 		rb2d.velocity = new Vector2(Horizontal * speed, rb2d.velocity.y);
 
 
 		RaycastHit2D hitInfo1 = Physics2D.Raycast (groundcheckleft.transform.position, Vector2.down, groundCheckTolerance, LayerMask.GetMask("Ground"));
 		RaycastHit2D hitInfo2 = Physics2D.Raycast (groundcheckright.transform.position, Vector2.down, groundCheckTolerance, LayerMask.GetMask("Ground"));
-		if (hitInfo1||hitInfo2.collider != null) {
-			if (Input.GetButtonDown ("Jump"))
-			{
-				rb2d.AddForce (Vector2.up * Jump); 
+		if (hitInfo1.collider != null || hitInfo2.collider != null) {
+			animator.SetBool ("grounded", true);
 
+			if (Input.GetButtonDown ("Jump")) {
+				rb2d.AddForce (Vector2.up * Jump); 
 			}
 		
-		
-
-
-		
+		} else {
+			animator.SetBool ("grounded", false);
 		}
+
+
 
 		bool flipSprite = (spriteRenderer.flipX ? (Horizontal > 0f) : (Horizontal < 0f));
 		if (flipSprite) 
@@ -52,7 +60,7 @@ public class CharacterMovement : MonoBehaviour {
 
 		}
 
-
+		//animator.SetBool ("Ground", Ground)
 
 	}
 }
